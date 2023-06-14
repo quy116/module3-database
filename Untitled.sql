e57d1a588dc6196b977e5bf0dc3c2cc204d4a5c7
quy116 <nguyenphucquy1106@gmail.com>
2023-06-14 13:40:13 +0700

    hello

--- Untitled.sql
+++
@@ -1,8 +0,0 @@
-SELECT * FROM `student-management`.class;
-insert into class(id,name)
-value (02,"module3");
-insert into class(id,name)
-value (01,"module3");
-insert into class(id,name)
-value (5,"module5");
-UPDATE `student-management`.`class` SET `id` = '1', `name` = 'module1' WHERE (`id` = '1');
---
+++ ss1/Untitled.sql
@@ -0,0 +1,3 @@
+CREATE SCHEMA `student-management` ; 
+create table class(id int primary key auto_increment, name varchar(50));
+create table Teacher(id int primary key,name varchar(50),age int,country varchar(50));
