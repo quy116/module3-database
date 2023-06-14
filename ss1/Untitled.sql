edc4406da804667a6043bc95a8cc8ddab778e768
quy116 <nguyenphucquy1106@gmail.com>
2023-06-14 14:01:44 +0700

    hello

--- ss1/Untitled.sql
+++ ss1/Untitled.sql
@@ -1,3 +1,9 @@
 CREATE SCHEMA `student-management` ; 
-create table class(id int primary key auto_increment, name varchar(50));
-create table Teacher(id int primary key,name varchar(50),age int,country varchar(50));
+create table class(id int primary key , name varchar(50));
+insert into class (id,name)
+values(12,"C0023G1"),(8,"C0123G1"),(6,"C0223G1"),(7,"C0323G1");
+create table Teacher(id int primary key ,name varchar(50),age int,country varchar(50));
+insert into Teacher(id,name,age,country)
+values(10,"trung",20,"VN");
+
+
