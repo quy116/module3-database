6e50c2f674e921220e8b95c6d21d620e57922b94
quy116 <nguyenphucquy1106@gmail.com>
2023-06-14 09:52:36 +0700

    hello

---
+++ Untitled.sql
@@ -0,0 +1,8 @@
+SELECT * FROM `student-management`.class;
+insert into class(id,name)
+value (02,"module3");
+insert into class(id,name)
+value (01,"module3");
+insert into class(id,name)
+value (5,"module5");
+UPDATE `student-management`.`class` SET `id` = '1', `name` = 'module1' WHERE (`id` = '1');
