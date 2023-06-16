ba8518484a7013fdb4a5d6cdf045eb525615e87b
quy116 <nguyenphucquy1106@gmail.com>
2023-06-16 16:48:27 +0700

    hello

--- Untitled.sql
+++ Untitled.sql
@@ -6,7 +6,10 @@ select startDate from class
 wHERE MONTH(StartDate) = 12;
 SELECT * FROM subject 
 WHERE credit BETWEEN 3 AND 5;
+SET SQL_SAFE_UPDATES = 0;
 select classId,StudentName from student;
-	
 update Student set ClassId = 2 where StudentName = "Hung";
-select classId,SubName,Mark from student;
+select studentName,SubName,Mark from student
+join mark on student.studentid = mark.studentid
+join subject on mark.subId = subject.subid
+oRDER BY Mark desc, StudentName asc;
--- ss2/bai2.sql
+++ ss2/bai2.sql
@@ -31,3 +31,30 @@ CREATE TABLE order_detail (
         REFERENCES product (product_id),
     PRIMARY KEY (order_id , product_id)
 );
+insert into customer
+values
+(1,"Minh Quân",10),
+(2,"Ngoc Oanh", 20),
+(3,"Hong Ha",50);
+insert into `order`
+values
+(1,1,"2006-03-21",null),
+(2,2,"2006-03-23",null),
+(3,1,"2006-03-16",null);
+insert into product
+values
+(1,"May Giat",3),
+(2,"Tu Lanh",5),
+(3,"Dieu Hoa",7),
+(4,"Quat",1),
+(5,"Bep Dien",2);
+insert into order_detail
+values
+(1,1,3),
+(1,3,7),
+(1,4,2),
+(2,1,1),
+(3,1,8),
+(2,5,4),
+(2,3,3);
+select order_id, order_date,  order_total_price from `order` 
